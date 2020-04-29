2e682e550da22a9be0f97fbe51bf3f7c78f74719
Roger Lo <rogerlo@emotibot.com.cn>
2020-04-30 00:15:27 +0800

    use localstorage to save data, add features

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
+Vue.config.devtools = true;
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!',
-      vueCanvas: null,
       origin: [0, 0],
       cards: 0,
       steps: 0,
@@ -13,47 +12,84 @@ var app = new Vue({
       repeat: 1
     },
     mounted() {
-      var canvas = document.getElementById("canvas");
-      canvas.height = window.innerHeight;
-      canvas.width = window.innerWidth;
-      var ctx = canvas.getContext("2d");  
-      this.vueCanvas = ctx;
-      this.origin = [100, 500];
+      if (localStorage.cardConfig) {
+        cardConfig = JSON.parse(localStorage.getItem('cardConfig'));
+        this.cardConfig = cardConfig;
+      }
+      if (localStorage.cards) {
+        this.cards = parseInt(localStorage.cards, 10);
+      }
+      if (localStorage.steps) {
+        this.steps = parseInt(localStorage.steps, 10);
+      }
+      if (localStorage.triSize) {
+        this.triSize = parseInt(localStorage.triSize, 10);
+      }
+      if (localStorage.getItem('rules')) {
+        try {
+          this.rules = JSON.parse(localStorage.getItem('rules'));
+        } catch(e) {
+          localStorage.removeItem('rules');
+        }
+      }
+      if (localStorage.repeat) {
+        this.repeat = parseInt(localStorage.repeat, 10);
+      }
     },
     methods: {
-      drawTri(pos, card, color, up, left, size){
+      saveConfig(){
+        localStorage.cards = this.cards;
+        localStorage.steps = this.steps;
+        localStorage.cardConfig = JSON.stringify(this.cardConfig);
+        localStorage.triSize = this.triSize;
+        localStorage.rules = JSON.stringify(this.rules);
+        localStorage.repeat = this.repeat;
+      },
+      resetAll(){
+        this.origin = [0, 0];
+        this.cards = 0;
+        this.steps = 0;
+        this.cardConfig = [];
+        this.triSize = 30;
+        this.stepConfig = [];
+        this.rules = [];
+        this.repeat = 1;
+        this.saveConfig()
+      },
+      drawTri(vueCanvas, pos, card, color, up, left, size){
         longSide = size;
         shortSide = size / 1.5;
         startX = this.origin[0] + ((card-1) * shortSide) + (1 *(card-1));
         startY = this.origin[1] - ((pos) * longSide);
-        this.vueCanvas.beginPath();
+        vueCanvas.beginPath();
         if (up == true){
-          this.vueCanvas.moveTo(startX, startY);
+          vueCanvas.moveTo(startX, startY);
           if(left == true){
-            this.vueCanvas.lineTo(startX, startY - longSide);
+            vueCanvas.lineTo(startX, startY - longSide);
           }else{
-            this.vueCanvas.lineTo(startX+shortSide, startY - longSide);
+            vueCanvas.lineTo(startX+shortSide, startY - longSide);
           }
-          this.vueCanvas.lineTo(startX + shortSide, startY);
+          vueCanvas.lineTo(startX + shortSide, startY);
         }else{
-          this.vueCanvas.moveTo(startX, startY - longSide);
+          vueCanvas.moveTo(startX, startY - longSide);
           if(left == true){
-            this.vueCanvas.lineTo(startX, startY);
+            vueCanvas.lineTo(startX, startY);
           }else{
-            this.vueCanvas.lineTo(startX + shortSide, startY);
+            vueCanvas.lineTo(startX + shortSide, startY);
           }
-          this.vueCanvas.lineTo(startX + shortSide, startY - longSide);
+          vueCanvas.lineTo(startX + shortSide, startY - longSide);
         }
-        this.vueCanvas.lineWidth = 2;
-        this.vueCanvas.strokeStyle = "black";
-        this.vueCanvas.stroke();
-        this.vueCanvas.closePath();
-        this.vueCanvas.fillStyle = color;
-        this.vueCanvas.fill();
+        vueCanvas.lineWidth = 2;
+        vueCanvas.strokeStyle = "black";
+        vueCanvas.stroke();
+        vueCanvas.closePath();
+        vueCanvas.fillStyle = color;
+        vueCanvas.fill();
       },
       clearCanvas(){
         var canvas = document.getElementById("canvas");
-        this.vueCanvas.clearRect(0, 0, canvas.width, canvas.height);
+        vueCanvas = canvas.getContext("2d");
+        vueCanvas.clearRect(0, 0, canvas.width, canvas.height);
       },
       drawBox(){
         // draw box
@@ -65,24 +101,25 @@ var app = new Vue({
         canvas.height = height + 50;
         canvas.width = width + 50;
         this.origin = [Lmargin+1, height+Tmargin-2];
-        this.vueCanvas = canvas.getContext("2d");
-        this.vueCanvas.strokeStyle = "black";
-        this.vueCanvas.strokeRect(Lmargin, Tmargin, width, height);
-        this.vueCanvas.stroke();
+        vueCanvas = canvas.getContext("2d");
+        vueCanvas.strokeStyle = "black";
+        vueCanvas.strokeRect(Lmargin, Tmargin, width, height);
+        vueCanvas.stroke();
         
         // draw step text
-        this.vueCanvas.font = "15px Arial";
+        vueCanvas.font = "15px Arial";
         for(var i = 0; i < this.steps*this.repeat; i++) {
-          this.vueCanvas.fillText(i+1, 2, height-((i+1)*this.triSize));
+          vueCanvas.fillText(i+1, 2, height-((i+1)*this.triSize));
         };
 
         // draw card text
-        this.vueCanvas.font = "13px Arial";
+        vueCanvas.font = "13px Arial";
         for(var i = 0; i < this.cards; i++ ) {
-          this.vueCanvas.fillText(i+1, i*this.triSize/1.5+i+Lmargin+2, height+Tmargin+20);
+          vueCanvas.fillText(i+1, i*this.triSize/1.5+i+Lmargin+2, height+Tmargin+20);
         };
+        return vueCanvas;
       },
-      drawStep(){
+      drawStep(vueCanvas){
         cardColorIdx = [];
         for(var i=0; i<this.cards; i++){
           cardColorIdx.push(0);
@@ -96,7 +133,7 @@ var app = new Vue({
             left = false;
           }
           up = false
-          this.drawTri(0, i+1, this.cardConfig[i].color[cardColorIdx[i]%4], up, left, this.triSize);
+          this.drawTri(vueCanvas, 0, i+1, this.cardConfig[i].color[cardColorIdx[i]%4], up, left, this.triSize);
           initCardLeft[i] = !left;
         }
         // draw steps
@@ -104,7 +141,7 @@ var app = new Vue({
           for (var i = 0; i < this.steps; i++){
             for (var j = 0; j < this.cards; j++){
               currentLeft = !(initCardLeft[j]^this.stepConfig[i][j]);
-              this.drawTri((r*this.steps)+i+1, j+1, this.cardConfig[j].color[cardColorIdx[j]%4], true, currentLeft, this.triSize);
+              this.drawTri(vueCanvas, (r*this.steps)+i+1, j+1, this.cardConfig[j].color[cardColorIdx[j]%4], true, currentLeft, this.triSize);
               if(this.stepConfig[i][j] == true){
                 cardColorIdx[j] += 1;
               }else{
@@ -113,8 +150,17 @@ var app = new Vue({
               if(cardColorIdx[j]<0){
                 cardColorIdx[j] = 4 + cardColorIdx[j]
               }
-              this.drawTri((r*this.steps)+i+1, j+1, this.cardConfig[j].color[cardColorIdx[j]%4], false, !currentLeft, this.triSize);
+              this.drawTri(vueCanvas, (r*this.steps)+i+1, j+1, this.cardConfig[j].color[cardColorIdx[j]%4], false, !currentLeft, this.triSize);
             }
+          }
+        }
+      },
+      resetStep(){
+        this.stepConfig = [];
+        for (var i = 0; i < this.steps; i++) {
+          this.stepConfig[i] = [];
+          for (var j = 0; j < this.cards; j++) {
+            this.stepConfig[i][j] = true;
           }
         }
       },
@@ -126,19 +172,15 @@ var app = new Vue({
             "color": ["#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"]
           };
         }
-        this.stepConfig = [];
-        for (var i = 0; i < this.steps; i++) {
-          this.stepConfig[i] = [];
-          for (var j = 0; j < this.cards; j++) {
-            this.stepConfig[i][j] = true;
-          }
-        }
+        this.resetStep();
       },
       addRule(){
         this.rules.push({
           card: 0,
-          from: 0,
-          to: 0
+          cardFrom: 0,
+          cardTo: 0,
+          stepFrom: 0,
+          stepTo: 0
         })
       },
       removeRule(index){
@@ -147,8 +189,10 @@ var app = new Vue({
       evalRules(){
         for (var i = 0; i < this.rules.length; i++){
           card = this.rules[i].card;
-          for(var j = this.rules[i].from; j <= this.rules[i].to; j++){
-            this.stepConfig[j][card] = false;
+          for(var j = this.rules[i].stepFrom; j<=this.rules[i].stepTo; j++){
+            for(var k = this.rules[i].cardFrom; k<=this.rules[i].cardTo; k++){
+              this.stepConfig[j][k] = false;
+            }
           }
         }
       },
@@ -156,10 +200,12 @@ var app = new Vue({
         console.log(this.cardConfig);
         console.log(this.stepConfig);
         console.log(this.rules);
+        this.resetStep();
         this.evalRules();
         this.clearCanvas();
-        this.drawBox();
-        this.drawStep();
+        var vueCanvas = this.drawBox();
+        this.drawStep(vueCanvas);
+        this.saveConfig();
       }
     }
   })
